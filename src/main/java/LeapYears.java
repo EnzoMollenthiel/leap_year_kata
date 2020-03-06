dff1f6384d85978f8be6244691c1407e15f39b43
enzo <mollenthielenzo@gmail.com>
2020-03-06 12:25:41 +0100

    green

--- src/main/java/LeapYears.java
+++ src/main/java/LeapYears.java
@@ -1,6 +1,6 @@
 public class LeapYears {
 
     public boolean isLeapYear(int year) {
-        return year % 400 == 0;
+        return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
     }
 }
