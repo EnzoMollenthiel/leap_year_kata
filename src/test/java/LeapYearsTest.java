31f1d4b8663286b23e3e3014d30c6dd41329c8db
enzo <mollenthielenzo@gmail.com>
2020-03-06 12:25:19 +0100

    red

--- src/test/java/LeapYearsTest.java
+++ src/test/java/LeapYearsTest.java
@@ -16,6 +16,13 @@ public class LeapYearsTest {
     public void isLeapYear_should_return_false_if_year_is_dividable_by_100_but_not_by_400() {
         int year = 2100;
 
-        assertEquals("Should return true if year is dividable by 400", false, learYears.isLeapYear(year));
+        assertEquals("Should return true if year is dividable by 100 but not by 400", false, learYears.isLeapYear(year));
+    }
+
+    @Test
+    public void isLeapYear_should_return_true_if_year_is_dividable_by_4_but_not_by_100() {
+        int year = 2008;
+
+        assertEquals("Should return true if year is dividable by 4 but not by 100", true, learYears.isLeapYear(year));
     }
 }
